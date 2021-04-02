a2ad4fe8925f551bc968180ebd612ce41a3c4edb
YashKandalkar <yashsk.yk@gmail.com>
2021-04-02 23:15:03 +0530

    feat: add ssl true

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const db = knex({
   client: "pg",
   connection:
     process.env.DEV !== "true"
-      ? process.env.DATABASE_URL
+      ? process.env.DATABASE_URL + "?ssl=true"
       : {
           host: "127.0.0.1",
           user: "postgres",
